cd5290f3bef7f30ee6cda062f5c32f00a6625aef
SaidBahromzoda <123372897+SaidBahromzoda@users.noreply.github.com>
2023-02-06 15:43:25 +0600

    1 commit

---
+++ Desktop/XcodeProjects/Weather_api_06.02/Weather_api_06.02/Weather_api_06_02App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_api_06_02App.swift
+//  Weather_api_06.02
+//
+//  Created by Apple on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_api_06_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
